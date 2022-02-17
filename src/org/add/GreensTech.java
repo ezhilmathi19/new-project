e9da81b22a80f1aeee98ac2b72ad44283e1fcdfb
ezhilmathi19 <ezhilvinay@gmail.com>
2022-02-17 09:46:07 +0530

    demo

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+
+	public void greensOmr() {
+		System.out.println ("GREENSOMR");
+		
+	}
+	public void greensAdayar() {
+		System.out.println(" GREENSADYAR");
+		
+	}
+	public void greensTambaram() {
+		System.out.println("GREENSTAMBARAM");
+		
+	}
+	public void greenseVelacherry() {
+		System.out.println("GREENSVELACHERRY");
+		
+	}
+	public void greensAnnanagar() {
+		System.out.println("GREENSANNANAGAR");
+	}
+	
+	public static void main (String [ ] args ) {
+		GreensTech g = new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greenseVelacherry();
+		g.greensAnnanagar();
+	}
+}
+		
+		
